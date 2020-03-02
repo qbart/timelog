8d00979a0e87a8538eeff070dd311a62e7e91435
qbart <wojtowicz.bartlomiej@gmail.com>
2020-03-03 00:49:45 +0100

    Restore diff based on tokenizer

--- timelog/diff_printer_test.go
+++ timelog/diff_printer_test.go
@@ -1,12 +1,13 @@
 package timelog
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_DiffPrinter_String(t *testing.T) {
+func Test_DiffPrinter_String_WithoutStop(t *testing.T) {
 	eventsOriginal := []event{
 		event{
 			name:    "start",
@@ -49,147 +50,97 @@ func Test_DiffPrinter_String(t *testing.T) {
 	assert.Equal(t, expectedResult, result)
 }
 
-// func Test_DiffPrinter_String_Empty(t *testing.T) {
-// 	p := DiffPrinter{
-// 		timeloggerOriginal: &TimeLogger{events: []event{}},
-// 		timeloggerModified: &TimeLogger{events: []event{}},
-// 	}
+func Test_DiffPrinter_String_WithStop(t *testing.T) {
+	eventsOriginal := []event{
+		event{
+			name:    "start",
+			comment: "hello",
+			at:      makeTime("2020-01-15 22:00"),
+		},
+		event{
+			name:    "stop",
+			comment: "",
+			at:      makeTime("2020-01-15 22:05"),
+		},
+	}
+	eventsModified := []event{
+		event{
+			name:    "start",
+			comment: "hello",
+			at:      makeTime("2020-01-15 21:59"),
+		},
+		event{
+			name:    "stop",
+			comment: "",
+			at:      makeTime("2020-01-15 22:10"),
+		},
+	}
 
-// 	result := p.String()
+	p := DiffPrinter{
+		timeloggerOriginal: &TimeLogger{events: eventsOriginal},
+		timeloggerModified: &TimeLogger{events: eventsModified},
+	}
 
-// 	expectedResult := ""
+	result := p.String()
 
-// 	assert.Equal(t, expectedResult, result)
-// }
+	expectedResult := trimHeredoc(`
+	-2020-01-15 [22:00] [22:05] hello
+	+2020-01-15 [21:59] [22:10] hello
+	`)
 
-// func Test_DiffPrinter_String_WithUnfinished(t *testing.T) {
-// 	eventsOriginal := []event{
-// 		event{
-// 			comment: "hello",
-// 			from: logtime{
-// 				finished: true,
-// 				t:        makeTime("2020-01-15 22:00"),
-// 			},
-// 			to: logtime{
-// 				finished: true,
-// 				t:        makeTime("2020-01-15 22:05"),
-// 			},
-// 		},
-// 		event{
-// 			comment: "world",
-// 			from: logtime{
-// 				finished: true,
-// 				t:        makeTime("2020-01-15 22:05"),
-// 			},
-// 			to: logtime{
-// 				finished: false,
-// 				t:        makeTime("2020-01-15 22:15"),
-// 			},
-// 		},
-// 	}
-// 	eventsModified := []event{
-// 		event{
-// 			comment: "hello",
-// 			from: logtime{
-// 				finished: true,
-// 				t:        makeTime("2020-01-15 22:00"),
-// 			},
-// 			to: logtime{
-// 				finished: true,
-// 				t:        makeTime("2020-01-15 22:10"),
-// 			},
-// 		},
-// 		event{
-// 			comment: "world",
-// 			from: logtime{
-// 				finished: true,
-// 				t:        makeTime("2020-01-15 22:10"),
-// 			},
-// 			to: logtime{
-// 				finished: false,
-// 				t:        makeTime("2020-01-15 22:15"),
-// 			},
-// 		},
-// 	}
+	assert.Equal(t, expectedResult, result)
+}
 
-// 	p := DiffPrinter{
-// 		timeloggerOriginal: &TimeLogger{events: eventsOriginal},
-// 		timeloggerModified: &TimeLogger{events: eventsModified},
-// 	}
+func Test_DiffPrinter_String_Empty(t *testing.T) {
+	p := DiffPrinter{
+		timeloggerOriginal: &TimeLogger{events: []event{}},
+		timeloggerModified: &TimeLogger{events: []event{}},
+	}
 
-// 	result := p.String()
+	result := p.String()
 
-// 	expectedResult := trimHeredoc(`
-// 	-2020-01-15 22:00 [22:05] hello
-// 	+2020-01-15 22:00 [22:10] hello
-// 	-2020-01-15 [22:05] ...   world
-// 	+2020-01-15 [22:10] ...   world
-// 	`)
+	expectedResult := ""
 
-// 	assert.Equal(t, expectedResult, result)
-// }
+	assert.Equal(t, expectedResult, result)
+}
 
-// func Test_DiffPrinter_String_NoChanges(t *testing.T) {
-// 	eventsOriginal := []event{
-// 		event{
-// 			comment: "hello",
-// 			from: logtime{
-// 				finished: true,
-// 				t:        makeTime("2020-01-15 22:00"),
-// 			},
-// 			to: logtime{
-// 				finished: true,
-// 				t:        makeTime("2020-01-15 22:05"),
-// 			},
-// 		},
-// 		event{
-// 			comment: "world",
-// 			from: logtime{
-// 				finished: true,
-// 				t:        makeTime("2020-01-15 22:05"),
-// 			},
-// 			to: logtime{
-// 				finished: false,
-// 				t:        makeTime("2020-01-15 22:15"),
-// 			},
-// 		},
-// 	}
-// 	eventsModified := []event{
-// 		event{
-// 			comment: "hello",
-// 			from: logtime{
-// 				finished: true,
-// 				t:        makeTime("2020-01-15 22:00"),
-// 			},
-// 			to: logtime{
-// 				finished: true,
-// 				t:        makeTime("2020-01-15 22:05"),
-// 			},
-// 		},
-// 		event{
-// 			comment: "world",
-// 			from: logtime{
-// 				finished: true,
-// 				t:        makeTime("2020-01-15 22:05"),
-// 			},
-// 			to: logtime{
-// 				finished: false,
-// 				t:        makeTime("2020-01-15 22:15"),
-// 			},
-// 		},
-// 	}
+func Test_DiffPrinter_String_NoChanges(t *testing.T) {
+	eventsOriginal := []event{
+		event{
+			name:    "start",
+			comment: "hello",
+			at:      makeTime("2020-01-15 22:00"),
+		},
+		event{
+			name:    "start",
+			comment: "world",
+			at:      makeTime("2020-01-15 22:05"),
+		},
+	}
+	eventsModified := []event{
+		event{
+			name:    "start",
+			comment: "hello",
+			at:      makeTime("2020-01-15 22:00"),
+		},
+		event{
+			name:    "start",
+			comment: "world",
+			at:      makeTime("2020-01-15 22:05"),
+		},
+	}
 
-// 	p := DiffPrinter{
-// 		timeloggerOriginal: &TimeLogger{events: eventsOriginal},
-// 		timeloggerModified: &TimeLogger{events: eventsModified},
-// 	}
+	p := DiffPrinter{
+		timeloggerOriginal: &TimeLogger{events: eventsOriginal},
+		timeloggerModified: &TimeLogger{events: eventsModified},
+	}
 
-// 	result := p.String()
+	result := p.String()
 
-// 	expectedResult := fmt.Sprint(
-// 		" 2020-01-15 22:00 22:05 hello\n",
-// 		" 2020-01-15 22:05 ...   world",
-// 	)
+	expectedResult := fmt.Sprint(
+		" 2020-01-15 22:00 22:05 hello\n",
+		" 2020-01-15 22:05 ...   world",
+	)
 
-// 	assert.Equal(t, expectedResult, result)
-// }
+	assert.Equal(t, expectedResult, result)
+}
